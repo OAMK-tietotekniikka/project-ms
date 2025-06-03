b8e8c2d390c4f007ac7171d392fadaefe342ff55
ldtrees <dstring2@proton.me>
2025-06-03 15:56:41 +0300

    Minor Fixes & Tooling Updates (#123)
    * fix: fix form format
    
    * feat (#89): change class_code parsing logic
    
    * feat (#89): fix structure to correct format, add validators (basic)
    
    * fix: direct pool.query -> connection.query
    
    * fix: minor struct fix
    
    * fix: fix sql queries to match students table struct
    
    * chore: change newStudent return struct
    
    * chore: comment to move to a diff section
    
    * chore: remove ;
    
    * build: add validator
    
    * chore: add students route, minor fixes/improvements
    
    * chore: add whitespace
    
    * fix: minor fix
    
    * feat(#89): add validation (for email, class code), some refactoring
    
    * build: dev setup, husky, biome, commitlint, lint-staged
    
    * chore(#122): add Docker setup with hot reload
    
    * chore(#122): configs for dev tools
    
    * chore(#122): makefile and script for easier dev setup
    
    * chore(#122): husky config
    
    * chore: update env
    
    * fix(db): db comment fix

---
+++ frontend/src/views/Students.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import ImportStudents from '../components/TeacherUI/ImportStudents';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+const Students: React.FC = () => {
+    const { t } = useTranslation();
+    const [showImportModal, setShowImportModal] = useState(false);
+
+    return (
+        <Container className='teacher-main-container'>
+            <Row style={{ width: "100%" }}>
+                <Col xs={12} md={12} lg={11}>
+
+                    <div className='d-flex justify-content-between align-items-center mt-4'>
+                        <Button
+                            className='addCompany-button'
+                            onClick={() => setShowImportModal(true)}
+                        >
+                            {t('importStudents')}
+                        </Button>
+                    </div>
+
+                    <Modal
+                        show={showImportModal}
+                        onHide={() => setShowImportModal(false)}
+                        size="lg"
+                        aria-labelledby="import-students-modal"
+                        centered
+                    >
+                        <Modal.Header closeButton>
+                            <Modal.Title>{t('importStudentsToClass')}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <ImportStudents handleClose={() => setShowImportModal(false)} />
+                        </Modal.Body>
+                    </Modal>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Students;
