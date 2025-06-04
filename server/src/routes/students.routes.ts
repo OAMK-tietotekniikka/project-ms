aa6f9d3c0708333cee6f09178ca4fe17793668d9
ldtrees <dstring2@proton.me>
2025-06-04 20:05:01 +0300

    feat(#120): bulk student create/update route

--- server/src/routes/students.routes.ts
+++ server/src/routes/students.routes.ts
@@ -1,17 +1,21 @@
-import { Router } from "express";   
-import { createStudent, getStudents,getStudent,updateStudent,deleteStudent } from "../controllers/students.controller";
+import { Router } from "express";
+import {
+	createStudent,
+	deleteStudent,
+	getStudent,
+	getStudents,
+	updateStudent,
+} from "../controllers/students.controller";
+import { createMultipleStudents } from "../controllers/students_bulk.controller";
 
 const studentsRouter = Router();
 
-studentsRouter.route('/')
-    .get(getStudents)
-    .post(createStudent);
+studentsRouter.route("/").get(getStudents).post(createStudent);
 
-studentsRouter.route('/:student_id')
-    .put(updateStudent)
-    .delete(deleteStudent);
+studentsRouter.route("/batch-create").post(createMultipleStudents);
 
-studentsRouter.route('/:email')
-    .get(getStudent);
+studentsRouter.route("/:student_id").put(updateStudent).delete(deleteStudent);
+
+studentsRouter.route("/:email").get(getStudent);
 
 export default studentsRouter;
