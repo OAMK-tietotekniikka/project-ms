631fd787505ed63b519b4b86a5f565203898e0ea
ldtrees <dstring2@proton.me>
2025-06-06 00:08:57 +0300

    feat(#91): protect project routes

--- server/src/routes/projects.routes.ts
+++ server/src/routes/projects.routes.ts
@@ -10,23 +10,32 @@ import {
 	getStudentProjects,
 	updateProject,
 } from "../controllers/projects.controller";
+import { authenticate, requireRole } from "../middleware/auth";
 
 const projectsRouter = Router();
 
-projectsRouter.route("/").get(getProjects).post(createProject);
+projectsRouter
+	.route("/")
+	.get(authenticate, requireRole(["student"]), getProjects)
+	.post(authenticate, requireRole(["student"]), createProject);
 
-projectsRouter.route("/:project_id").put(updateProject).delete(deleteProject);
+projectsRouter
+	.route("/:project_id")
+	.put(authenticate, updateProject)
+	.delete(authenticate, deleteProject);
 
 projectsRouter
 	.route("/student/:student_id")
-	.get(getStudentProjects)
-	.post(createStudentProject);
+	.get(authenticate, getStudentProjects)
+	.post(authenticate, createStudentProject);
 
 projectsRouter
 	.route("/:project_id/notes")
-	.get(getProjectNotes)
-	.post(addProjectNote);
+	.get(authenticate, getProjectNotes)
+	.post(authenticate, addProjectNote);
 
-projectsRouter.route("/:project_id/notes/:note_id").delete(deleteProjectNote);
+projectsRouter
+	.route("/:project_id/notes/:note_id")
+	.delete(authenticate, deleteProjectNote);
 
 export default projectsRouter;
