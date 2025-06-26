440df5f6e8510c0538fe3b7fc3026dd01631be46
ldtrees <dstring2@proton.me>
2025-06-26 23:20:06 +0300

    feat!: update/fix sql queries

--- server/src/query/projects.query.ts
+++ server/src/query/projects.query.ts
@@ -1,18 +1,35 @@
 export const QUERY = {
-    SELECT_PROJECTS: `
-        SELECT * 
-        FROM projects 
-        ORDER BY created_at DESC 
-        LIMIT 50
+	SELECT_STUDENT_BY_EMAIL: `
+    SELECT student_id
+    FROM students
+    WHERE email = ?
     `,
 
-    SELECT_PROJECT: `
+	SELECT_TEACHER_BY_EMAIL: `
+    SELECT teacher_id
+    FROM teachers
+    WHERE email = ?
+    `,
+
+	SELECT_PROJECTS: `
+        SELECT 
+            p.project_id,
+            p.teacher_id,
+            p.project_name,
+            p.project_status,
+            t.teacher_name     
+        FROM projects p
+        LEFT JOIN teachers t ON p.teacher_id = t.teacher_id
+        
+    `,
+
+	SELECT_PROJECT: `
         SELECT * 
         FROM projects 
         WHERE project_id = ?
     `,
 
-    CREATE_PROJECT: `
+	CREATE_PROJECT: `
         INSERT INTO projects (
             project_name, 
             project_desc, 
@@ -26,12 +43,11 @@ export const QUERY = {
         VALUES (?, ?, ?, ?, ?, ?, ?, ?)
     `,
 
-    UPDATE_PROJECT: `
+	UPDATE_PROJECT: `
         UPDATE projects 
         SET 
             project_name = ?, 
             project_desc = ?, 
-            teacher_id = ?, 
             company_id = ?, 
             project_status = ?, 
             project_url = ?, 
@@ -40,34 +56,114 @@ export const QUERY = {
         WHERE project_id = ?
     `,
 
-    DELETE_PROJECT_BY_ID: `
+	GET_PROJECT_MEMBERS: `
+        SELECT s.student_id, s.student_name
+        FROM (
+            SELECT student_id FROM student_project WHERE project_id = ?
+             ) AS filtered_projects
+        JOIN students s ON filtered_projects.student_id = s.student_id
+    `,
+
+	UPDATE_PROJECT_STATUS: `
+    UPDATE projects
+    SET project_status = ? WHERE project_id = ?
+    `,
+
+	DELETE_PROJECT_BY_ID: `
         DELETE FROM projects 
         WHERE project_id = ?
     `,
 
-    SELECT_STUDENT_PROJECTS: `
+	SELECT_STUDENT_PROJECTS: `
         SELECT * 
         FROM student_project 
         ORDER BY student_id ASC
     `,
 
-    SELECT_STUDENT_PROJECTS_BY_STUDENT_ID: `
-        SELECT 
-            p.project_id, 
-            p.project_name, 
-            p.project_status, 
-            p.start_date, 
-            p.end_date 
-        FROM projects p 
-        JOIN (
-            SELECT project_id 
-            FROM student_project 
-            WHERE student_id = ?
-        ) sp_filtered 
-        ON sp_filtered.project_id = p.project_id
+	SELECT_STUDENT_PROJECTS_BY_STUDENT_ID: `
+        SELECT
+            p.project_id,
+            p.project_desc,
+            p.project_url,
+            p.company_id,
+            p.teacher_id,
+            p.project_name,
+            p.project_status,
+            p.start_date,
+            p.end_date,
+            c.company_name,
+            t.teacher_name
+        FROM projects p
+                 INNER JOIN student_project sp ON p.project_id = sp.project_id
+                 INNER JOIN teachers t ON p.teacher_id = t.teacher_id
+                 LEFT JOIN companies c ON p.company_id = c.company_id
+        WHERE sp.student_id = ?
     `,
 
-    CREATE_STUDENT_PROJECT: `
+	SELECT_STUDENT_PROJECTS_BY_TEACHER_ID: `
+        SELECT
+            p.project_id,
+            p.project_desc,
+            p.project_url,
+            p.company_id,
+            p.teacher_id,
+            p.project_name,
+            p.project_status,
+            p.start_date,
+            p.end_date,
+            c.company_name,
+            t.teacher_name
+        FROM projects p
+                 INNER JOIN teachers t ON p.teacher_id = t.teacher_id
+                 LEFT JOIN companies c ON p.company_id = c.company_id
+        WHERE p.teacher_id = ?
+    `,
+
+	SELECT_STUDENT_PROJECT_BY_PROJECT_ID: `
+        SELECT
+            p.project_id,
+            p.project_desc,
+            p.project_url,
+            p.company_id,
+            p.teacher_id,
+            p.project_name,
+            p.project_status,
+            p.start_date,
+            p.end_date,
+            c.company_name,
+            t.teacher_name
+        FROM projects p
+                 LEFT JOIN teachers t ON p.teacher_id = t.teacher_id
+                 LEFT JOIN companies c ON p.company_id = c.company_id
+        WHERE p.project_id = ?
+    `,
+
+	SELECT_STUDENT_PROJECT_BY_STUDENT_AND_PROJECT_ID: `
+        SELECT
+            p.project_id,
+            p.project_desc,
+            p.project_url,
+            p.company_id,
+            p.teacher_id,
+            p.project_name,
+            p.project_status,
+            p.start_date,
+            p.end_date,
+            c.company_name,
+            t.teacher_name
+        FROM projects p
+                 LEFT JOIN teachers t ON p.teacher_id = t.teacher_id
+                 LEFT JOIN companies c ON p.company_id = c.company_id
+        WHERE p.project_id = ?
+          AND EXISTS (
+            SELECT 1
+            FROM student_project sp
+            WHERE sp.project_id = p.project_id
+              AND sp.student_id = ?
+        )
+    `,
+
+	CREATE_STUDENT_PROJECT: `
         INSERT INTO student_project (
             student_id, 
             project_id, 
@@ -76,7 +172,7 @@ export const QUERY = {
         VALUES (?, ?, ?)
     `,
 
-    INSERT_PROJECT_NOTE: `
+	INSERT_PROJECT_NOTE: `
         INSERT INTO project_note (
             project_id, 
             note, 
@@ -86,31 +182,61 @@ export const QUERY = {
         VALUES (?, ?, ?, ?)
     `,
 
-    SELECT_PROJECT_NOTES: `
+	SELECT_PROJECT_NOTES: `
         SELECT * 
         FROM project_note 
         WHERE project_id = ?
     `,
 
-    DELETE_PROJECT_NOTE: `
+	DELETE_PROJECT_NOTE: `
         DELETE FROM project_note 
         WHERE note_id = ? 
         AND project_id = ?
     `,
 
-    DELETE_STUDENT_PROJECT: `
+	DELETE_STUDENT_PROJECT: `
         DELETE FROM student_project 
         WHERE student_id = ? 
         AND project_id = ?
     `,
 
-    DELETE_STUDENT_PROJECT_BY_PROJECT_ID: `
+	DELETE_STUDENT_PROJECT_BY_PROJECT_ID: `
         DELETE FROM student_project 
         WHERE project_id = ?
     `,
 
-    DELETE_PROJECT_NOTES_BY_PROJECT_ID: `
+	DELETE_PROJECT_NOTES_BY_PROJECT_ID: `
         DELETE FROM project_note 
         WHERE project_id = ?
-    `
+    `,
+
+	SELECT_PROJECTS_BY_COMPANY_AND_YEAR: `
+    SELECT 
+    c.company_name,
+    COUNT(p.project_id) AS amount_of_projects,
+    CONCAT(
+        YEAR(p.start_date), 
+        '-', 
+        YEAR(p.end_date), 
+        ' ', 
+        CASE 
+            WHEN MONTH(p.start_date) BETWEEN 1 AND 6 THEN 'Spring'
+            ELSE 'Fall'
+        END
+    ) AS study_semester
+    FROM projects p
+    JOIN companies c ON p.company_id = c.company_id
+    GROUP BY 
+        c.company_name,
+        YEAR(p.start_date),
+        YEAR(p.end_date),
+        CASE 
+            WHEN MONTH(p.start_date) BETWEEN 1 AND 6 THEN 'Spring'
+        ELSE 'Fall'
+    END
+ORDER BY 
+    c.company_name, 
+    study_semester;
+
+    `,
 };
--- server/src/query/resources.query.ts
+++ server/src/query/resources.query.ts
@@ -1,11 +1,21 @@
 export const R_QUERY = {
-    SELECT_RESOURCES: 'SELECT * FROM resources ORDER BY created_at DESC LIMIT 50',
-    SELECT_RESOURCE: 'SELECT * FROM resources WHERE resource_id = ?',
-    CREATE_RESOURCE: 'INSERT INTO resources (teacher_id, total_resources, used_resources, study_year) VALUES (?, ?, ?, ?)',
-    UPDATE_RESOURCE: 'UPDATE resources SET teacher_id = ?, total_resources = ?, used_resources = ?, study_year = ? WHERE resource_id = ?',
-    DELETE_RESOURCE: 'DELETE FROM resources WHERE resource_id = ?',
+	SELECT_RESOURCES: "SELECT * FROM resources ORDER BY created_at DESC LIMIT 50",
+	SELECT_RESOURCE: "SELECT * FROM resources WHERE resource_id = ?",
+	CREATE_RESOURCE:
+		"INSERT INTO resources (teacher_id, total_resources, used_resources, study_year) VALUES (?, ?, 0, ?)",
+	UPDATE_RESOURCE:
+		"UPDATE resources SET total_resources = ? WHERE resource_id = ?",
+	DELETE_RESOURCE: "DELETE FROM resources WHERE resource_id = ?",
 
-    SELECT_RESOURCES_BY_STUDY_YEAR: `
+	SELECT_INDIVIDUAL_TEACHER_RESOURCES: `SELECT 
+    resource_id,
+    used_resources, 
+    total_resources, 
+    study_year
+    FROM resources
+    WHERE teacher_id = ?`,
+
+	SELECT_RESOURCES_BY_STUDY_YEAR: `
         SELECT r.*, t.teacher_name, t.email 
         FROM resources r
         JOIN teachers t ON r.teacher_id = t.teacher_id
@@ -14,20 +24,20 @@ export const R_QUERY = {
         ORDER BY r.used_resources ASC
     `,
 
-    SELECT_TEACHERS_WITH_COMPANY_FAVORITE: `
+	SELECT_TEACHERS_WITH_COMPANY_FAVORITE: `
         SELECT tc.teacher_id
         FROM company_teacher tc
         JOIN companies c ON tc.company_id = c.company_id
         WHERE c.company_name = ?
     `,
 
-    INCREMENT_RESOURCE_USAGE: `
+	INCREMENT_RESOURCE_USAGE: `
         UPDATE resources 
         SET used_resources = used_resources + 1 
         WHERE teacher_id = ? AND study_year = ? AND used_resources < total_resources
     `,
 
-    DECREMENT_RESOURCE_USAGE: `
+	DECREMENT_RESOURCE_USAGE: `
         UPDATE resources 
         SET used_resources = used_resources - 1 
         WHERE teacher_id = ? AND study_year = ? AND used_resources > 0
