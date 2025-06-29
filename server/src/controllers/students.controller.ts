56a9c7cdc1b248d024728660077987bd5f14293f
ldtrees <dstring2@proton.me>
2025-06-30 02:51:22 +0300

    feat: update student controllers, remove old ones, add docs, validation

--- server/src/controllers/students.controller.ts
+++ server/src/controllers/students.controller.ts
@@ -1,6 +1,14 @@
+/**
+ * Students controller.
+ * Manages creating, reading, updating, and deleting student records.
+ *
+ * @version 2.0.0
+ * @since 29.06.2025
+ * @module
+ */
+
 import { Request, Response } from "express";
 import pool from "../config/mysql.config";
-import { HttpResponse } from "../domain/response";
 import {
 	ResultSetHeader,
 	RowDataPacket,
@@ -12,6 +20,8 @@ import { responseHelper } from "../domain/newResponse";
 import { logRequests } from "../utils/logRequests";
 import { logError } from "../utils/logError";
 import { z } from "zod";
+import { AuthenticatedRequest } from "../middleware/auth";
+import { getStudentIdByEmail } from "../utils/getUsersByEmail";
 
 const emailValidation = z.object({
 	email: z.string().email(),
@@ -19,7 +29,6 @@ const emailValidation = z.object({
 
 const updateStudentSchema = z
 	.object({
-		email: z.string().email().min(8).max(100).optional(),
 		student_name: z.string().min(4).max(100).optional(),
 		class_code: z.string().optional(),
 	})
@@ -51,6 +60,12 @@ function getChangedFields(
 	return changedFields;
 }
 
+/**
+ * Retrieves all student records.
+ *
+ * Fetches and returns all student records from the database.
+ */
+
 export const getStudents = async (
 	req: Request,
 	res: Response,
@@ -74,46 +89,21 @@ export const getStudents = async (
 	}
 };
 
-export const getStudent = async (
-	req: Request,
-	res: Response,
-): Promise<void> => {
-	logRequests(req);
-
-	const parseEmail = emailValidation.safeParse(req.params);
-	if (!parseEmail.success) {
-		responseHelper.badRequest(res);
-		return;
-	}
-	const { email } = parseEmail.data;
-
-	let connection: PoolConnection | null = null;
-	try {
-		connection = await pool.getConnection();
-		const [student] = await connection.query<RowDataPacket[]>(
-			QUERY.SELECT_STUDENT_BY_EMAIL,
-			[email],
-		);
-		if (student.length > 0) {
-			responseHelper.ok(res, student);
-			return;
-		}
-		responseHelper.notFound(res);
-		return;
-	} catch (error: unknown) {
-		logError("getStudent", error);
-		throw error;
-	} finally {
-		if (connection) connection.release();
-	}
-};
-
+/**
+ * Creates a new student record.
+ *
+ * Adds a new student to the database and returns the created or existing record with ID and timestamp.
+ */
 export const createStudent = async (
-	req: Request,
+	req: AuthenticatedRequest,
 	res: Response,
 ): Promise<void> => {
 	logRequests(req);
-	let studentRaw = createStudentSchema.safeParse(req.body);
+	let studentRaw = createStudentSchema.safeParse({
+		student_name: req.user?.name,
+		email: req.user?.email,
+		class_code: req.body.class_code,
+	});
 	let connection: PoolConnection | null = null;
 
 	if (!studentRaw.success) {
@@ -135,8 +125,8 @@ export const createStudent = async (
 		}
 
 		const [q] = await connection.query<ResultSetHeader>(QUERY.CREATE_STUDENT, [
-			student_name,
-			email,
+			student_name?.toLowerCase(),
+			email?.toLowerCase(),
 			class_code || null,
 		]);
 		const id = q.insertId;
@@ -156,19 +146,29 @@ export const createStudent = async (
 	}
 };
 
+/**
+ * Updates an existing student record.
+ *
+ * Modifies student data using provided fields, updating only changed values. Returns the updated record.
+ */
 export const updateStudent = async (
-	req: Request,
+	req: AuthenticatedRequest,
 	res: Response,
 ): Promise<void> => {
 	logRequests(req);
 	let connection: PoolConnection | null = null;
 	try {
 		connection = await pool.getConnection();
+		const id = await getStudentIdByEmail(connection, req.user?.email || "");
+		if (!id) {
+			responseHelper.notFound(res);
+			return;
+		}
 
 		const [student] = await connection.query<RowDataPacket[]>(
 			QUERY.SELECT_STUDENT,
-			[req.params.student_id],
-		);
+			[id],
+		); // TODO efficiency (too many db runs)
 		if (student.length === 0) {
 			responseHelper.notFound(res);
 			return;
@@ -201,8 +201,7 @@ export const updateStudent = async (
 		await connection.query(updateQuery, queryValues);
 		responseHelper.ok(res); // can add message with student id so teachers will be notified
 		return;
-
-    } catch (error: unknown) {
+	} catch (error: unknown) {
 		logError("updateStudent", error);
 		responseHelper.internalServerError(res);
 		return;
@@ -211,9 +210,14 @@ export const updateStudent = async (
 	}
 };
 
-// all data, !TBD
+// all data, !TODO
+/**
+ * Deletes a student record and related data.
+ *
+ * Deletes a student by ID, including all associated data. Returns a success/error response.
+ */
 export const deleteStudent = async (
-	req: Request,
+	req: AuthenticatedRequest,
 	res: Response,
 ): Promise<void> => {
 	logRequests(req);
@@ -233,6 +237,45 @@ export const deleteStudent = async (
 		return;
 	} catch (error: unknown) {
 		logError("deleteStudent", error);
+		responseHelper.internalServerError(res);
+		return;
+	} finally {
+		if (connection) connection.release();
+	}
+};
+
+/**
+ * Retrieves a student record using a token.
+ *
+ * description Fetches student data from the database using the provided token. Returns the student record.
+ */
+export const getStudentUpdated = async (
+	req: AuthenticatedRequest,
+	res: Response,
+): Promise<void> => {
+	logRequests(req);
+	let connection: PoolConnection | null = null;
+
+	try {
+		connection = await pool.getConnection();
+		const student_id = await getStudentIdByEmail(
+			connection,
+			req.user?.email || "",
+		);
+		if (!student_id) {
+			responseHelper.unauthorized(res);
+			return;
+		}
+
+		const [student] = await connection.query<RowDataPacket[]>(
+			QUERY.SELECT_STUDENT,
+			[student_id],
+		);
+
+		responseHelper.ok(res, student);
+		return;
+	} catch (error: unknown) {
+		logError("updatedStudentReturnData", error);
 		responseHelper.internalServerError(res);
 		return;
 	} finally {
