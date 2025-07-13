135f49b5d1bd12e70393a0763495c961fe80d28b
ldtrees <dstring2@proton.me>
2025-07-13 20:23:56 +0300

    chore: kebab case -> snake case

--- server/src/shared/utils/user_email_lookup.ts
+++ server/src/shared/utils/user_email_lookup.ts
@@ -1,4 +1,3 @@
-
 import { QUERY } from "../../features/projects/queries/projects.query";
 import mariadb from "mariadb";
 
@@ -6,10 +5,7 @@ export const getStudentIdByEmail = async (
 	connection: mariadb.PoolConnection,
 	email: string,
 ): Promise<number | null> => {
-	const rows = await connection.query(
-		QUERY.SELECT_STUDENT_BY_EMAIL,
-		[email],
-	);
+	const rows = await connection.query(QUERY.SELECT_STUDENT_BY_EMAIL, [email]);
 	if (rows.length === 0 || !rows[0].student_id) {
 		return null;
 	}
@@ -20,10 +16,7 @@ export const getTeacherIdByEmail = async (
 	connection: mariadb.PoolConnection,
 	email: string,
 ): Promise<number | null> => {
-	const rows = await connection.query(
-		QUERY.SELECT_TEACHER_BY_EMAIL,
-		[email],
-	);
+	const rows = await connection.query(QUERY.SELECT_TEACHER_BY_EMAIL, [email]);
 	if (rows.length === 0 || !rows[0].teacher_id) {
 		return null;
 	}
