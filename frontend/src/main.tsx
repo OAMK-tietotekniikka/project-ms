ce250f196a010f8e24753ba66fd5c75843109e79
ldtrees <dstring2@proton.me>
2025-07-19 15:55:43 +0300

    feat: cleaner logic, refactor, new auth, protection, nesting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,28 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { I18nextProvider } from "react-i18next";
-import i18n from "./i18next";
+import i18n from "@/locales/i18next";
 import { BrowserRouter } from "react-router-dom";
-import { MsalProvider } from "@azure/msal-react";
-import { PublicClientApplication } from "@azure/msal-browser";
-import { msalConfig } from "./authConfig";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/core/ThemeProvider";
+import { Toaster } from "@/shared/components/ui/sonner";
 import "./index.css";
-import { Toaster } from "@/components/ui/sonner";
+import { MsalProvider } from "@azure/msal-react";
+import { msalInstance } from "@/core/auth/auth";
 
-const msalInstance = new PublicClientApplication(msalConfig);
+const root = ReactDOM.createRoot(
+	document.getElementById("root") as HTMLElement,
+);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+root.render(
 	<React.StrictMode>
-		<MsalProvider instance={msalInstance}>
-			<BrowserRouter>
+		<BrowserRouter>
+			<MsalProvider instance={msalInstance}>
 				<I18nextProvider i18n={i18n}>
-					<ThemeProvider defaultTheme="dark" storageKey="theme">
+					<ThemeProvider>
 						<App />
 						<Toaster position="top-center" duration={1500} />
 					</ThemeProvider>
 				</I18nextProvider>
-			</BrowserRouter>
-		</MsalProvider>
+			</MsalProvider>
+		</BrowserRouter>
 	</React.StrictMode>,
 );
