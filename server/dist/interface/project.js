8e7271bf13128e75f821e9ec0410c5d04814296e
ldtrees <dstring2@proton.me>
2025-07-19 15:39:38 +0300

    chore: stop dist track

--- server/dist/interface/project.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//project_name, project_desc, teacher_id, company_id, project_status, project_url, start_date, end_date
