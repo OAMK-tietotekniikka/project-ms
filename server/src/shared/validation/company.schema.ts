f842caf5d55dfc28e6cc3458b29b977bcaa9054d
ldtrees <dstring2@proton.me>
2025-08-07 19:04:03 +0300

    chore: improve schema validations

--- server/src/shared/validation/company.schema.ts
+++ server/src/shared/validation/company.schema.ts
@@ -2,5 +2,13 @@ import { z } from "zod";
 
 export const companyIdParamsSchema = z.coerce.number().int().positive();
 export const companyCreateSchema = z.object({
-	company_name: z.string().min(2),
+	company_name: z
+		.string()
+		.min(2)
+		.max(255)
+		.transform((val) => val.toLowerCase()),
+	industry: z
+		.string()
+		.optional()
+		.transform((val) => val?.toLowerCase()),
 });
